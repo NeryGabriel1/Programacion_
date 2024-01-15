0ae54417007b626dc8e600c7b0aa8d9659207561
Nery Benitez <nerycabral406@gmail.com>
2024-01-16 00:37:25 +0100

    Ejercicio 3

---
+++ EjerPoo/src/POO/Bicicleta.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class Bicicleta {
+	// Atributos:
+	int numRuedas;
+	String tipo;
+	int tamaño;
+
+	// Método Constructor:
+	public Bicicleta() {
+		numRuedas = 2;
+		tipo = "Montaña";
+		tamaño = 26;
+
+	}
+
+}
---
+++ EjerPoo/src/POO/Use_Bicicleta.java
@@ -0,0 +1,13 @@
+package POO;
+
+public class Use_Bicicleta {
+
+	public static void main(String[] args) {
+		Bicicleta sport_r = new Bicicleta();
+
+		System.out.println("Me han regalado una bicicleta para " + sport_r.tipo
+				+ " que tiene unas ruedas super buenas com un aro de " + sport_r.tamaño + ".");
+
+	}
+
+}
