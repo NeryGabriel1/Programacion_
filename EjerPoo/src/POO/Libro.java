d2fd2844b6b466044b15ca85414e72e95d05756d
Nery Benitez <nerycabral406@gmail.com>
2024-01-16 00:32:59 +0100

    Ejercicio 2

---
+++ EjerPoo/src/POO/Libro.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class Libro {
+	// Atributos:
+	String titulo;
+	String autor;
+	int numPaginas;
+
+	// Método Constructor:
+	public Libro() {
+		titulo = "Sin nombre";
+		autor = "Agusto Roa Bastos";
+		numPaginas = 100;
+
+	}
+
+}
---
+++ EjerPoo/src/POO/Use_Libro.java
@@ -0,0 +1,12 @@
+package POO;
+
+public class Use_Libro {
+
+	public static void main(String[] args) {
+		Libro Mi_mundo = new Libro();
+
+		System.out.println("He comprado un libro del autor" + Mi_mundo.autor + " que se llama " + Mi_mundo.titulo + ".");
+
+	}
+
+}
