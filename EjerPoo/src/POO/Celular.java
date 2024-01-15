f1e735b31f179cfbd3f430d7db8bbbd28d07f053
Nery Benitez <nerycabral406@gmail.com>
2024-01-16 00:44:49 +0100

    Ejercicio 5

---
+++ EjerPoo/src/POO/Celular.java
@@ -0,0 +1,16 @@
+package POO;
+
+public class Celular {
+	// Atributos:
+	String modelo;
+	int almacenamiento;
+	int camaraMP;
+
+	// Método Constructor:
+	public Celular() {
+		modelo = "Xaomi";
+		almacenamiento = 64;
+		camaraMP = 12;
+
+	}
+}
---
+++ EjerPoo/src/POO/Use_Celular.java
@@ -0,0 +1,13 @@
+package POO;
+
+public class Use_Celular {
+
+	public static void main(String[] args) {
+		Celular cel = new Celular();
+
+		System.out.println("Me han regalado un telefono de la marca " + cel.modelo
+				+ " com una capacidad de almacenamiento de " + cel.almacenamiento + " Gb.");
+
+	}
+
+}
