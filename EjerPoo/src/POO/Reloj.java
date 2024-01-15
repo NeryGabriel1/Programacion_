556275f5e6e13da0378890995d65ecabb8596347
Nery Benitez <nerycabral406@gmail.com>
2024-01-16 00:39:43 +0100

    Ejercico 4

---
+++ EjerPoo/src/POO/Reloj.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class Reloj {
+	// Atributos:
+	String marca;
+	boolean digital;
+	double precio;
+
+	// - Método Constructor:
+	public Reloj() {
+		marca = "Casio";
+		digital = true;
+		precio = 50.0;
+
+	}
+
+}
---
+++ EjerPoo/src/POO/Use_Reloj.java
@@ -0,0 +1,12 @@
+package POO;
+
+public class Use_Reloj {
+
+	public static void main(String[] args) {
+		Reloj x = new Reloj();
+
+		System.out.println("Hr comprado un reglo de " + x.precio + " de la marca " + x.marca + ".");
+
+	}
+
+}
