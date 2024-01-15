70d2573584f94f9abb0bf469495c7951adf89d5f
Nery Benitez <nerycabral406@gmail.com>
2024-01-16 00:28:45 +0100

    Ejercio 1

---
+++ EjerPoo/src/POO/Coche.java
@@ -0,0 +1,21 @@
+package POO;
+
+public class Coche {
+	// 1)
+	int motor;
+	int ruedas;
+	int largo;
+	int ancho;
+	int peso;
+
+	// 2) creo un metodo contructor que es un metodo especial donde se inicia todo
+	// el metodo, se tiene que usar el mismo nombre de la clase
+	 public Coche() { 
+		 ancho = 250;
+		 largo=400;
+		 peso=1900;
+		 motor=200;
+		 ruedas=4;
+	 }
+
+}
---
+++ EjerPoo/src/POO/Use_coche.java
@@ -0,0 +1,15 @@
+package POO;
+
+public class Use_coche {
+
+	public static void main(String[] args) {
+		// esto es intancciar una clase
+		// Esto es crear una clase
+		Coche Renaul = new Coche();
+
+		// ahora solo quiero las caracteristicas del coche
+		System.out.println("MI coche  tiene " + Renaul.ruedas + " ruedas");
+
+	}
+
+}
